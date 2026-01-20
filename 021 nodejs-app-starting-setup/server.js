48603de4a4eea34b00906a27845ce5c487f1ce28
shahbaz-kamal <shahbazkamal384@gmail.com>
2026-01-20 12:01:17 +0600

    wathced lecture 44

--- 021 nodejs-app-starting-setup/server.js
+++ 021 nodejs-app-starting-setup/server.js
@@ -44,5 +44,5 @@ app.post("/store-goal", (req, res) => {
 });
 
 app.listen(5000, "0.0.0.0", () => {
-  console.log("Server is running on port 5000");
+  console.log("Server is running on port 5000 in full swing");
 });
